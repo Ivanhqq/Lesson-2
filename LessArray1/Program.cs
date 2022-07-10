b3278c4c79e2006583046ecc3d9e2f56c9060dd7
Ivan <ivanazanov@gmail.com>
2022-07-10 15:09:22 +0500

    Нахождение макс с массивом

--- LessArray1/Program.cs
+++ LessArray1/Program.cs
@@ -11,8 +11,14 @@ int Max(int arg1, int arg2, int arg3)
 //№эл. масс.    0  1    2  3  4  5  6    7   8   
 int[] array = {23, 45, 24, 8, 9, 0, 12, 456, 256};
 
+int result = Max (
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
 
-Console.WriteLine(array[8]);
+Console.WriteLine(result);
+
+
 
 
 
