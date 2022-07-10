60e37060ab372e16a49899f36f0802ec8b7732ae
Ivan <ivanazanov@gmail.com>
2022-07-10 15:28:19 +0500

    Нахождение номера эл массива

---
+++ LessArrayFunc2/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = {23, 45, 24, 8, 256, 0, 12, 456, 256};
+
+int n = array.Length;
+
+Console.Clear ();
+Console.WriteLine("ВВеди целое число и получишь № эл массива");
+int find = Convert.ToInt32(Console.ReadLine());
+//int find = 45;
+
+int index = 0;
+
+while (index < n)
+{
+      if(array[index] == find)
+      {
+        Console.WriteLine(index); 
+        break;
+      }
+      index++;
+    
+}
