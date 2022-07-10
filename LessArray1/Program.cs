f253b8d8ef700fe4390f4d9610f27b545875f5b4
Ivan <ivanazanov@gmail.com>
2022-07-10 15:06:03 +0500

    добавил массив

---
+++ LessArray1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int Max(int arg1, int arg2, int arg3)
+{
+   int result = arg1;
+   if (arg2 > result) result = arg2;
+   if (arg3 > result) result = arg3;
+   return result;
+
+}
+
+//№эл. масс.    0  1    2  3  4  5  6    7   8   
+int[] array = {23, 45, 24, 8, 9, 0, 12, 456, 256};
+
+
+Console.WriteLine(array[8]);
+
+
+
+
